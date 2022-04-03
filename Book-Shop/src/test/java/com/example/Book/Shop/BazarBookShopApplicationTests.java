0b033420562aa299611c9148c1d3b33e1df11470
yousefdheede <58173109+yousefdheede@users.noreply.github.com>
2022-04-03 12:24:17 +0300

    Add files via upload

---
+++ Book-Shop/src/test/java/com/example/Book/Shop/BazarBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Book.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
